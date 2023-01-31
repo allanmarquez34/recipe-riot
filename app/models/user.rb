d22abf977c04c5ac5386b194fe99ba7d0e6977ff
Allan Marquez <allan.marquez.2003@icloud.com>
2023-01-31 16:20:32 -0700

    new stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
     has_many :likes
     has many :follows
 
-    has_many :liked_posts trough: :likes, source: :posts
-    has_many :commented_posts trough: :comments, source: :posts
-    has_many :bookmarked_posts trough: :bookmarks, source: :posts
+    has_many :liked_posts, through: :likes, source: :posts
+    has_many :commented_posts, through: :comments, source: :posts
+    has_many :bookmarked_posts, through: :bookmarks, source: :posts
    
 
 end
